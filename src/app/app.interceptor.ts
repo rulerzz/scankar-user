1cc038afbd6816a87c30524b9fa058ad21b8d8c6
rulerzz <may.ghz.conan@gmail.com>
2021-03-07 11:40:36 +0530

    ok

--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -1,17 +1,28 @@
 import { AppService } from './app.service';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { EMPTY } from 'rxjs';
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
   logged: any;
   currentTime: any;
-  constructor(private _snackBar: MatSnackBar, private router: Router, private appservice: AppService){
-  }
-  intercept(req: HttpRequest<any>,next: HttpHandler): Observable<HttpEvent<any>> {
+  constructor(
+    private _snackBar: MatSnackBar,
+    private router: Router,
+    private appservice: AppService
+  ) {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     if (req.url.includes('login') || req.url.includes('register')) {
       return next.handle(req);
     } else {
@@ -22,7 +33,10 @@ export class AppInterceptor implements HttpInterceptor {
       this.currentTime = new Date();
       let hours = Math.abs(this.currentTime - this.logged) / 36e5;
       if (hours > 3) {
-        localStorage.clear();
+        localStorage.removeItem('token');
+        localStorage.removeItem('role');
+        localStorage.removeItem('id');
+        localStorage.removeItem('time');
         this.router.navigate(['login']);
         return next.handle(req);
       } else {
@@ -39,5 +53,5 @@ export class AppInterceptor implements HttpInterceptor {
         }
       }
     }
-}
+  }
 }
